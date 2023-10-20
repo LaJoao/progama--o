9cd1ebebff72783b10268626dbb7a973f011675b
joao <joaovitorfidelisdeoliveira@gmail.com>
2023-10-19 22:32:57 -0300

    aula 1 js

---
+++ script.js
@@ -0,0 +1,43 @@
+//let pessoa1 = 'Matheus';//Essa variavel pode ser reatribuida
+//var pessoa2 = 'Alan';//Esse tipo de variavel, não e mais utilizado como boas práticas no java
+//const pessoa3 = 'Kaique';//a variavel do tipo const, só pode ser atribuida uma única vez
+
+//let mensagem = 'Olá, essa é a mensagem da minha variável'
+//console.log(mensagem);
+//mensagem = 'Essa é a nova mensagem da minha variável';
+//console.log(mensagem);
+
+//float: números que contém pontos ou vírgulas
+//int: números inteiros(não contém pontos ou vírgulas)
+//bool: booleano: tuer(verdadedeito), false(falso)
+//string: do tipo texto, precisa ser colocado entere aspas simpres ou duplas
+
+let num1 = 5;
+let num2 = 20;
+
+//let soma = num1 + num2;
+//console.log(soma);
+
+//let multi = num1 * num2;
+//console.log(multi);
+
+//let divisao = num1 / num2;
+//console.log(divisao);
+
+//let sub = num1 - num2;
+//console.log(sub);
+
+//if: se
+//else: senão
+
+// > : maior que
+// < : menor que
+// >= : maior ou igual
+// <= : menor ou igual
+// === : igual
+
+if(num1 >10){
+    console.log('O número 1 é maior do que 10')
+}else{
+    console.log('O numero 1, não e maior do que 10')
+}
